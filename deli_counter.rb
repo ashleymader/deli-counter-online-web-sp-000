1aa43c79f53572c3ee17528ec33d8297b330a65c
Ashley M Mader <ashley.mader12@gmail.com>
2020-02-23 16:50:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,4 +20,6 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
 
-    
+def now_serving(katz_deli)
+  puts "The line is currently serving #{katz_deli[0]}."
+end
