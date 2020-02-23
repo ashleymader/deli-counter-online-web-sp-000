8ad0f4463ba3b3303f040a9759a629d461f91f8e
Ashley M Mader <ashley.mader12@gmail.com>
2020-02-23 16:34:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  output_message = "";
+  if katz_deli.size == 0 
+    puts "The line is currently empty."
+  else 
+    line_number = 1 
+    katz_deli.each do |name|
+      output_message << " #{line_number}. #{name}"
+      line_number += 1 
+     end
+  puts "The line is currently:#{output_message}"
+end
+end
